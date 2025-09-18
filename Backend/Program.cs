a2413b6090dc1ae99275d5583a47b7c076d1002f
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2025-09-18 15:06:34 +0300

    added: finished initailazing all the code of dependency intergration

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,10 @@
+using Backend.DatabasContext;
+using Backend.Repository.Implementation;
+using Backend.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +14,34 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//JSON Serializer
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(
+    options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+//Dependancy injection
+builder.Services.AddDbContext<ApplicationDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DeliveryMatchString")));
+
+// Register ITrying with its implementation
+builder.Services.AddScoped<IAddress,AddressRepository>();
+builder.Services.AddScoped<ICustomer, CustomerRepository>();
+builder.Services.AddScoped<IDocuments, DocumentRepository>();
+builder.Services.AddScoped<IDriver, DriverRepository>();
+builder.Services.AddScoped<IEarnings, EarningRepository>();
+builder.Services.AddScoped<INotifications, NotificationRepository>();
+builder.Services.AddScoped<IOrder_Items, OrderItemRepository>();
+builder.Services.AddScoped<IOrder_Placement, OrderPLacementRepository>();
+builder.Services.AddScoped<IOrder_Tracking, OrderTrackingRepository>();
+builder.Services.AddScoped<IPayment, PaymentRepository>();
+builder.Services.AddScoped<IRoute, RouteRepository>();
+builder.Services.AddScoped<IUser, UserRepository>();
+builder.Services.AddScoped<IVehicle, VehicleRepository>();
+
 var app = builder.Build();
+
+//Enable CORS ~ so that the service can be consumed from the front end of the project
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
