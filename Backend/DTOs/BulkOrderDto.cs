82f49aa76946f68b09b39300f21610d73ae72575
Ben Clyde Sikanwe <151398987+RockingWithTheBest@users.noreply.github.com>
2025-11-17 02:53:39 +0300

    Add : Added BulkOrderDto models

---
+++ Backend/DTOs/BulkOrderDto.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models;
+
+namespace Backend.DTOs
+{
+    public class BulkOrderDto
+    {
+        public List<OrderPlacement> OrdersPlacmentsDto { get; set; }
+        public List<Order_Items> OrderItemsDto { get; set; }
+        public int ClientId { get; set; }
+    }
+}
