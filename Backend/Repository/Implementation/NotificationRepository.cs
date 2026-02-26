41c1e4cc60478712daf3375607179fb66b0c37cd
Ben Clyde Sikanwe <151398987+RockingWithTheBest@users.noreply.github.com>
2026-02-26 17:17:52 +0300

    feat : Addition of Two new important methods GetAllNotificationsPlacedByCustomer and also GetAllNotificationsPlacedByDriver..also commenting out of the Title attribute in the GetSingleRecord method

--- Backend/Repository/Implementation/NotificationRepository.cs
+++ Backend/Repository/Implementation/NotificationRepository.cs
@@ -2,6 +2,7 @@
 using Backend.Models;
 using Backend.Repository.Implementation;
 using Backend.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
 using System.Net;
 
 namespace Backend.Repository.Implementation
@@ -55,6 +56,47 @@ namespace Backend.Repository.Implementation
             return databaseContext.Notifications.ToList();
         }
 
+        public IEnumerable<Notification> GetAllNotificationsPlacedByDriver(int DriverId)
+        {
+            var driverRecord = databaseContext
+                .Drivers
+                .Where(d=>d.Id == DriverId)
+                .Include(i=>i.NotificationsPlaced)
+                .FirstOrDefault();
+            if (driverRecord == null)
+            {
+                return Enumerable.Empty<Notification>();
+            }
+            else
+            {
+                return driverRecord.NotificationsPlaced;
+            }
+        }
+
+        public IEnumerable<Notification> GetAllNotificationsPlacedByCustomer(int CustomerId)
+        {
+            var customerRecord = databaseContext
+                .Customers
+                .Where(d => d.Id == CustomerId)
+                .Include(i=>i.NotificationsPlaced)
+                .FirstOrDefault();
+          
+            if (customerRecord == null)
+            {
+                return Enumerable.Empty<Notification>();
+            }
+            else
+            {
+                return customerRecord.NotificationsPlaced;
+            }
+        }
+
+        public IEnumerable<Customer> GetCustomersWithIds(List<int> CustomersList)
+        {
+            return databaseContext
+                .Customers
+                .Where(i => CustomersList.Contains(i.Id)).ToList();
+        }
         public Notification GetSingleRecord(int Id)
         {
             return databaseContext.Notifications.Where(temp => temp.Id == Id).FirstOrDefault();
@@ -71,9 +113,15 @@ namespace Backend.Repository.Implementation
             {
                 Notification updatedRecord = databaseContext.Notifications.Where(temp => temp.Id == Id).FirstOrDefault();
                 updatedRecord.Type = notify.Type;
-                updatedRecord.Title = notify.Title;
+                //updatedRecord.Title = notify.Title;
                 updatedRecord.Message = notify.Message;
                 updatedRecord.IsRead = notify.IsRead;
+                updatedRecord.DriverCommentry = notify.DriverCommentry;
+                updatedRecord.CreatedAt = notify.CreatedAt;
+                updatedRecord.IsRead = notify.IsRead;
+                updatedRecord.CustomerId = notify.CustomerId;
+                updatedRecord.DriverId = notify.DriverId;
+                updatedRecord.OrderPlacementId = notify.OrderPlacementId;
                 databaseContext.SaveChanges();
                 testValue = updatedRecord.Id;
             }
