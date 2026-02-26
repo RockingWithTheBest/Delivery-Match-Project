84f0800eab6a7746e8aaaeaafa7431a15f9998cb
Ben Clyde Sikanwe <151398987+RockingWithTheBest@users.noreply.github.com>
2026-02-26 17:12:08 +0300

    feat : Addition of a GeoCoordinates class that helps in working on the Anty Colony Optimization allgorithm

---
+++ Backend/Repository/Interface/IGeocodingService.cs
@@ -0,0 +1,35 @@
+﻿using Backend.DatabasContext;
+using Backend.Models;
+
+namespace Backend.Repository.Interface
+{
+    public interface IGeocodingService
+    {
+        Task<GeoCoordinate> GetCoordinatesAsync(int OrderId, CancellationToken ct = default);
+    }
+
+    // Mock implementation for development
+    public class MockGeocodingService : IGeocodingService
+    {
+        private readonly ApplicationDatabaseContext _context;
+        public MockGeocodingService(ApplicationDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Task<GeoCoordinate> GetCoordinatesAsync(int OrderId, CancellationToken ct = default)
+        {
+            var orderPlacements = _context.OrderPlacements.ToList();
+            var geoCoord = _context.OrderTrackings
+                .Where(o => o.OrderPlacementId == OrderId)
+                .FirstOrDefault();
+            
+            var arrayOfCoords = geoCoord.DeliveryLocation.Split(',');
+            return Task.FromResult<GeoCoordinate?>(new GeoCoordinate
+            {
+                Latitude = double.Parse(arrayOfCoords[0]),
+                Longitude = double.Parse(arrayOfCoords[1])
+            });
+        }
+    }
+}
