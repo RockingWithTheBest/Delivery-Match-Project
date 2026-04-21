5d24b03f18cdfe490d81ac8ff9c18240fd4ab4e1
Ben Clyde Sikanwe <151398987+RockingWithTheBest@users.noreply.github.com>
2026-04-21 14:15:31 +0300

    feat : JWL user login

---
+++ Backend/JWTImplementation/JWT/LoginUser.cs
@@ -0,0 +1,56 @@
+﻿using Backend.DatabasContext;
+using Backend.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Microservice.JWT
+{
+    public sealed class LoginUser
+    {
+        private readonly ApplicationDatabaseContext context;
+        private readonly IPasswordHasher<User> passwordHasher;
+        private readonly TokenProvider tokenProvider;
+
+        public LoginUser(ApplicationDatabaseContext context, IPasswordHasher<User> passwordHasher, TokenProvider tokenProvider)
+        {
+            this.context = context;
+            this.passwordHasher = passwordHasher;
+            this.tokenProvider = tokenProvider;
+        }
+
+        public sealed record Request(string Email, string Password);
+
+        public string Handle(Request request)
+        {
+            User user = context.Users.Where(i => i.Email == request.Email).FirstOrDefault();
+
+            if (user == null)   
+            {
+                throw new Exception("User not found");
+            }
+
+            if (string.IsNullOrEmpty(user.Email))
+            {
+                throw new Exception("The user doesn't contain an email address.");
+            }
+
+            // Verify password using IPasswordHasher
+            var result = passwordHasher.VerifyHashedPassword(user, user.Password, request.Password);
+            
+            if(result == PasswordVerificationResult.Failed)
+            {
+                throw new Exception("The password is incorrect");
+            }
+
+            // Optional: If password needs rehashing (outdated algorithm), update it
+            if (result == PasswordVerificationResult.SuccessRehashNeeded)
+            {
+                // Rehash the password with current algorithm
+                user.Password = passwordHasher.HashPassword(user, request.Password);
+                context.SaveChanges(); // Save the updated hash
+            }
+
+            string token = tokenProvider.Create(user);
+            return token;
+        }
+    }
+}
---
+++ Backend/Migrations/20260420122115_YOU.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class YOU : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalOrders",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalOrders",
+                table: "Customers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine",
+                table: "Addresses",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
